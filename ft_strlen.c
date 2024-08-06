65274fea59adc6972d29dc5237f3ed2278baba3e
rnomoto <r.nomoto12345@gmail.com>
2024-08-06 18:20:46 +0900

    main comment out

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 09:58:36 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/08/06 18:18:19 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/08/06 18:20:14 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,14 @@ size_t ft_strlen(const char *str)
 	}
 }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(void)
-{
-	const char *s1 = "abcde";
-	const char *s2 = "Hello World!";
-	const char *s3 = NULL;
-	printf("input: %s, output: %zu\n", s1, ft_strlen(s1));
-	printf("input: %s, output: %zu\n", s2, ft_strlen(s2));
-	printf("input: %s, output: %zu\n", s3, ft_strlen(s3));
-}
+// int main(void)
+// {
+// 	const char *s1 = "abcde";
+// 	const char *s2 = "Hello World!";
+// 	const char *s3 = NULL;
+// 	printf("input: %s, output: %zu\n", s1, ft_strlen(s1));
+// 	printf("input: %s, output: %zu\n", s2, ft_strlen(s2));
+// 	printf("input: %s, output: %zu\n", s3, ft_strlen(s3));
+// }
