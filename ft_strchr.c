1e16134063f61f76652b3e3caf90e0c3e42fc07c
rnomoto <r.nomoto12345@gmail.com>
2024-08-08 11:35:22 +0900

    strchr done

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 15:30:43 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/08/07 16:48:56 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/08/08 11:34:53 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,27 @@
 
 char *ft_strchr(const char* s, int c)
 {
-    char *result = NULL;
     while (*s)
     {
         if (*s == c)
         {
-            while (*s)
-            {
-                *result = *s;
-                result++;
-                s++;
-            }
-            *result = '\0';
-            return result;
+           return (char *)s;
         }
         s++;
     }
     return NULL;
 }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(void)
-{
-    char *input = "Hello World!";
-    char i = 'r';
-    char j = 'a';
+// int main(void)
+// {
+//     char *input = "Hello World!";
+//     char i = 'r';
+//     char j = 'a';
 
-    printf("input: %c, output: %s\n", i, ft_strchr(input, i));
-    printf("input: %c, output: %s\n", j, ft_strchr(input, i));
+//     printf("input: %c, output: %s\n", i, ft_strchr(input, i));
+//     printf("input: %c, output: %s\n", j, ft_strchr(input, j));
 
-    return 0;
-}
+//     return 0;
+// }
