dbd7b63aff4506605386f097e2796d2f8d560d1d
rnomoto <r.nomoto12345@gmail.com>
2024-08-21 15:15:17 +0900

    strlcpy done

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 13:29:58 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/08/21 14:33:53 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/08/21 15:14:30 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,35 +27,36 @@ size_t	ft_strlen(const char *str)
 	}
 }
 
-size_t ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-    int i = 0;
-    size_t result = 0;
+	int		i;
+	size_t	result;
 
-    result = ft_strlen(src);
-    
-    while (src[i] != '\0' && size > 1)
-    {
-        dst[i] = src[i];
-        i++;
-        size--;
-    }
-    dst[i] = '\0';
-
-    return result;
+	i = 0;
+	result = 0;
+	result = ft_strlen(src);
+	while (src[i] != '\0' && size > 1)
+	{
+		dst[i] = src[i];
+		i++;
+		size--;
+	}
+	dst[i] = '\0';
+	return (result);
 }
 
-int main(void)
-{
-    char *dst = "42Tokyo";
-    const char *src = "Hello World!";
+// int main(void)
+// {
+//     const char *src = "Hello World!";
+//     char *dst = (char *)malloc((sizeof(char)) * 10);
 
-    printf("dst(before): %s\n", dst);
-    printf("dstsize: %lu\n", sizeof(dst));
-    printf("src: %s\n", src);
-    size_t result = ft_strlcpy(dst, src, sizeof(dst));
-    printf("srcsize: %zu\n", result);
-    printf("dst(after): %s\n", dst);
+//     printf("dst(before): %s\n", dst);
+//     printf("dstsize: %lu\n", sizeof(dst));
+//     printf("src: %s\n", src);
+//     size_t result = ft_strlcpy(dst, src, sizeof(dst));
+//     printf("srcsize: %zu\n", result);
+//     printf("dst(after): %s\n", dst);
 
-    return 0;
-}
+//     free(dst);
+//     return (0);
+// }
