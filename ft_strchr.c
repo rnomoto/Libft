b9c1765025131e54e151e57a6be530e1a4f6c5e2
Ryosuke Nomoto <rnomoto@c1r7s4.42tokyo.jp>
2024-08-21 13:08:44 +0900

    headder file changed

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,11 +6,11 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 15:30:43 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/08/08 11:34:53 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/08/21 13:08:29 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
@@ -24,8 +24,6 @@ char	*ft_strchr(const char *s, int c)
 	}
 	return (NULL);
 }
-
-// #include <stdio.h>
 
 // int main(void)
 // {
