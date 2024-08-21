7e90fff739ea9180b3a8e014f70ee9b062763cde
Ryosuke Nomoto <rnomoto@c1r7s4.42tokyo.jp>
2024-08-21 13:11:12 +0900

    headder file changed

--- libft.h
+++ libft.h
@@ -6,13 +6,14 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 15:28:22 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/08/02 15:28:33 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/08/21 13:01:51 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 #define LIBFT_H
 
-
+#include <stdio.h>
+#include <stdlib.h>
 
 #endif
