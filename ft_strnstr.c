ce62b41a5384ab514c713824e64ea393d6e83ca1
rnomoto <r.nomoto12345@gmail.com>
2024-08-21 13:22:08 +0900

    normkilled

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,33 +6,34 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 12:26:10 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/08/21 13:09:47 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/08/21 13:20:59 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strnstr(const char *s1, const char *s2, size_t len)
+char	*ft_strnstr(const char *s1, const char *s2, size_t len)
 {
-    int i = 0;
-    int j = 0;
+	int	i;
+	int	j;
 
-    if (s1 == NULL)
-        return (char *)s1;
-
-    while (s1[i + j] != '\0' && len > 0)
-    {
-        j = 0;
-        while (s1[i + j] == s2[j] && s2[j] != '\0')
-        {
-            j++;
-        }
-        if (s2[j] == '\0')
-            return (char *)s1 + i;
-        i++;
-        len--;
-    }
-    return NULL;
+	i = 0;
+	j = 0;
+	if (s1 == NULL)
+		return ((char *)s1);
+	while (s1[i + j] != '\0' && len > 0)
+	{
+		j = 0;
+		while (s1[i + j] == s2[j] && s2[j] != '\0')
+		{
+			j++;
+		}
+		if (s2[j] == '\0')
+			return ((char *)s1 + i);
+		i++;
+		len--;
+	}
+	return (NULL);
 }
 
 // int main(void)
@@ -42,7 +43,7 @@ char *ft_strnstr(const char *s1, const char *s2, size_t len)
 //     const char *to_find = "lo";
 
 //     printf("input: %s, search: %s, output: %s\n", tmp1, to_find, ft_strnstr(tmp1, to_find, 10));
-//     printf("input: %s, search: %s, output: %s\n", tmp2, to_find, ft_strnstr(tmp2, to_find, 7));
+	//     printf("input: %s, search: %s, output: %s\n", tmp2, to_find, ft_strnstr(tmp2, to_find, 7));
 
-//     return 0;
-// }
+	//     return (0);
+	// }
