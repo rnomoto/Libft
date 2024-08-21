816f1e4fe9325385f23b89094587b5e023cc9271
rnomoto <r.nomoto12345@gmail.com>
2024-08-21 21:27:54 +0900

    strlcat done

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 11:36:00 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/08/21 21:22:20 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/08/21 21:27:15 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,47 +27,49 @@ size_t	ft_strlen(const char *str)
 	}
 }
 
-char *ft_strncat(char *dst, const char *src, size_t size)
+char	*ft_strncat(char *dst, const char *src, size_t size)
 {
-    size_t i = 0;
-    size_t j = 0;
+	size_t	i;
+	size_t	j;
 
-    while (dst[i] != '\0')
-        i++;
-    while (i + j < size - 1)
-    {
-        dst[i + j] = src[j];
-        j++;
-    }
-    dst[i + j] = '\0';
-
-    return dst;
+	i = 0;
+	j = 0;
+	while (dst[i] != '\0')
+		i++;
+	while (i + j < size - 1)
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	dst[i + j] = '\0';
+	return (dst);
 }
 
-size_t ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-    size_t dst_len = ft_strlen(dst);
-    size_t src_len = ft_strlen(src);
+	size_t	dst_len;
+	size_t	src_len;
 
-    ft_strncat(dst, src, size);
-
-    if (dst_len >= size)
-        return (size + dst_len);
-    else 
-        return (dst_len + src_len);
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	ft_strncat(dst, src, size);
+	if (dst_len >= size)
+		return (size + dst_len);
+	else
+		return (dst_len + src_len);
 }
 
-int main(void)
-{
-    char dst[100] = "42Tokyo.";
-    const char *src = "Hello World!";
-    size_t result = 0;
+// int main(void)
+// {
+//     char dst[100] = "42Tokyo.";
+//     const char *src = "Hello World!";
+//     size_t result = 0;
 
-    printf("dst: %s\n", dst);
-    printf("src: %s\n", src);
+//     printf("dst: %s\n", dst);
+//     printf("src: %s\n", src);
 
-    result = ft_strlcat(dst, src, 25);
-    printf("dst_cat: %s output: %zu\n", dst, result);
+//     result = ft_strlcat(dst, src, 25);
+//     printf("dst_cat: %s output: %zu\n", dst, result);
 
-    return 0;
-}
+//     return (0);
+// }
