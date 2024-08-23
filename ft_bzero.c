13ca1c68210089e0824de8147c34ee4176ffeead
rnomoto <r.nomoto12345@gmail.com>
2024-08-23 12:00:33 +0900

    bzero fixed

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,43 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   bzero.c                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 16:27:48 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/05/06 16:41:24 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/08/23 11:59:43 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
 
 void	*ft_bzero(void *str, size_t n)
 {
-	char	*dest;
+	size_t index = 0;
+	unsigned char *str_cast = (unsigned char *)str;
 
-	dest = str;
-	while (0 < n)
+	while (index < n)
 	{
-		dest[n - 1] = '0';
-		n--;
+		str_cast[index] = '0';
+		index++;
 	}
-	return (dest);
+	return (str_cast);
 }
 
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-//int	main(int argc, char **argv)
-//{
-//	char	*result;
-//
-//	if (argc != 3)
-//	{
-//		printf("Error!\n");
-//		return (1);
-//	}
-//	result = ft_bzero(argv[1], atoi(argv[2]));
-//	printf("%s\n", result);
-//	return (0);
-//}
+// int main(void)
+// {
+// 	char *str = (char *)malloc(20 * sizeof(char));
+// 	size_t n = 15;
+
+// 	ft_bzero(str, n);
+
+// 	printf("%s\n", str);
+
+// 	return 0;
+// }
