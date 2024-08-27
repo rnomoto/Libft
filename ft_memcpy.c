361ca24d7a8bc95aa5a7d7fe83065ef52123a48e
rnomoto <r.nomoto12345@gmail.com>
2024-08-27 22:12:52 +0900

    memcpy name changed. code still not fixed.

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,25 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   memcpy.c                                           :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 16:53:36 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/05/15 14:25:47 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/08/27 22:11:56 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	int	i;
-
-	while (0 < n)
+	size_t index = 0;
+	while (index < n)
 	{
-		dest[i] = src[i];
-		i++;
-		n--;
+		dst[index] = src[index];
 	}
 }
