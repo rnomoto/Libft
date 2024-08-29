7a515676fc0d69245961b7a0fa2677c99c0b960d
rnomoto <r.nomoto12345@gmail.com>
2024-08-29 17:17:47 +0900

    not '0'. you should put '^@'(NULL)

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 16:27:48 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/08/23 11:59:43 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/08/29 17:10:11 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 
 void	*ft_bzero(void *str, size_t n)
 {
-	size_t index = 0;
-	unsigned char *str_cast = (unsigned char *)str;
+	size_t			index;
+	unsigned char	*str_cast;
 
+	index = 0;
+	str_cast = (unsigned char *)str;
 	while (index < n)
 	{
-		str_cast[index] = '0';
+		str_cast[index] = 0;
 		index++;
 	}
-	return (str_cast);
+	return (str);
 }
 
 // int main(void)
@@ -34,5 +36,5 @@ void	*ft_bzero(void *str, size_t n)
 
 // 	printf("%s\n", str);
 
-// 	return 0;
+// 	return (0);
 // }
