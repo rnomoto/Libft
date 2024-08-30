c2418a158fab5172f96a5ff4dcfca2a064b1863a
rnomoto <r.nomoto12345@gmail.com>
2024-08-30 11:55:33 +0900

    you should search str until the end (null included) & handle if int c is like 'a'+256

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,30 +6,37 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 15:30:43 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/08/21 13:08:29 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/08/30 11:53:22 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *str, int c)
 {
-	while (*s)
+	int		index;
+	char	c_cast;
+
+	index = 0;
+	c_cast = (char)c;
+	while (str[index] != '\0')
 	{
-		if (*s == c)
+		if (str[index] == c_cast)
 		{
-			return ((char *)s);
+			return ((char *)str + index);
 		}
-		s++;
+		index++;
 	}
+	if (str[index] == c_cast)
+		return ((char *)str + index);
 	return (NULL);
 }
 
 // int main(void)
 // {
-//     char *input = "Hello World!";
-//     char i = 'r';
-//     char j = 'a';
+//     char *input = "teste";
+//     int i = 'e' + 256;
+//     char j = '\0';
 
 //     printf("input: %c, output: %s\n", i, ft_strchr(input, i));
 //     printf("input: %c, output: %s\n", j, ft_strchr(input, j));
