71e567bca8aad482579e77330bedf5c9d154d7ce
rnomoto <r.nomoto12345@gmail.com>
2024-08-30 15:23:58 +0900

    you should write function prototype

--- libft.h
+++ libft.h
@@ -6,14 +6,34 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 15:28:22 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/08/21 13:01:51 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/08/29 16:56:25 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <stdio.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *s1, const char *s2, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+
+void	*ft_memset(void *str, int c, size_t n);
+void	*ft_bzero(void *str, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif
