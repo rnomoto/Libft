a5c5f6e587a7cdf112b6a89174d34fb51549b65c
rnomoto <r.nomoto12345@gmail.com>
2024-09-01 12:55:09 +0900

    char is signed. you should cast to unsigned char if you want to compare

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 12:02:55 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/08/21 13:09:24 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/09/01 12:51:41 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,47 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	index;
+	size_t				count;
+	const unsigned char	*s1_cast = (const unsigned char *)s1;
+	const unsigned char	*s2_cast = (const unsigned char *)s2;
 
-	index = 0;
-	while ((s1[index] != '\0' && s2[index] != '\0') && index < n)
+	count = 0;
+	while ((*s1_cast && *s2_cast) && count < n)
 	{
-		if (s1[index] != s2[index])
-			return (1);
-		index++;
+		if (*s1_cast != *s2_cast)
+			return (*s1_cast - *s2_cast);
+		s1_cast++;
+		s2_cast++;
+		count++;
 	}
-	if ((s1[index] == '\0' || s2[index] == '\0') && s1[index] != s2[index])
-		return (1);
+	if ((*s1_cast == '\0' || *s2_cast == '\0') && (*s1_cast != *s2_cast
+			&& count < n))
+		return (*s1_cast - *s2_cast);
 	return (0);
 }
 
 // int main(void)
 // {
-//     char *s1 = "Hello";
-//     char *s2 = "Herlo";
-//     size_t i = 4;
-//     size_t j = 2;
+//     char *s1 = "\200";
+//     char *s2 = "\0";
+//     size_t i = 1;
+//     size_t j = 0;
 //     size_t k = 6;
 
-//     printf("s1: %s, s2: %s, num: %zu, return: %d\n", s1, s2, i,ft_strncmp(s1, s2, i));
-//     printf("s1: %s, s2: %s, num: %zu, return: %d\n", s1, s2, j,ft_strncmp(s1, s2, j));
-//     printf("s1: %s, s2: %s, num: %zu, return: %d\n", s1, s2, k,ft_strncmp(s1, s2, k));
+// 	int res1 = ft_strncmp(s1, s2, i);
+//     printf("mine s1: %s, s2: %s, num: %zu, return: %d\n", s1, s2, i, res1);
+// 	int ans1 = strncmp(s1, s2, i);
+// 	printf("answ s1: %s, s2: %s, num: %zu, return: %d\n", s1, s2, i, ans1);
+
+// 	int res2 = ft_strncmp(s1, s2, j);
+//     printf("mine s1: %s, s2: %s, num: %zu, return: %d\n", s1, s2, j, res2);
+// 	int ans2 = strncmp(s1, s2, j);
+// 	printf("answ s1: %s, s2: %s, num: %zu, return: %d\n", s1, s2, j, ans2);
+
+// 	int res3 = ft_strncmp(s1, s2, k);
+//     printf("mine s1: %s, s2: %s, num: %zu, return: %d\n", s1, s2, k, res3);
+// 	int ans3 = strncmp(s1, s2, k);
+// 	printf("answ s1: %s, s2: %s, num: %zu, return: %d\n", s1, s2, k, ans3);
 
 //     return (0);
 // }
