2641d81dd90da81c3b9f0c232bb425be5b12488a
rnomoto <r.nomoto12345@gmail.com>
2024-09-07 12:04:57 +0900

    if either of dest and src is NULL, the movement is undefined. but if both of them is NULL, you shoul return dest.

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 16:53:36 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/08/29 15:11:46 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/09/07 11:59:12 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 
 	index = 0;
 	dest_cast = (unsigned char *)dest;
+	if (dest == NULL && src == NULL)
+		return (dest);
 	while (index < n)
 	{
 		dest_cast[index] = src_cast[index];
@@ -30,10 +32,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 
 // int main(void)
 // {
-// 	char *dest = (char *)malloc(20 * sizeof(char));
+// 	//char *dest = (char *)malloc(20 * sizeof(char));
 // 	const unsigned char src[20] = "Hello World!";
 
-// 	unsigned char *result = ft_memcpy(dest, src, 7);
+// 	unsigned char *result = ft_memcpy((void *)0, (void *)0, 3);
 
 // 	printf("src: %s, dest: %s\n", src, result);
 
