6313742a3f76f9f7eb20ecf903dec025f17b7e43
rnomoto <r.nomoto12345@gmail.com>
2024-09-09 14:01:09 +0900

    how to reverse-copy the buffer

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,38 +6,46 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 12:12:31 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/09/09 12:38:26 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/09/09 13:59:59 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+void	*ft_memrcpy(void *dest, const void *src)
+{
+	size_t				index;
+	unsigned char		*dest_cast;
+	const unsigned char	*src_cast = (const unsigned char *)src;
+
+    index = ft_strlen(src);
+	dest_cast = (unsigned char *)dest;
+	if (dest == NULL && src == NULL)
+		return (dest);
+	while (index >= 0)
+	{
+		dest_cast[index] = src_cast[index];
+		index--;
+	}
+	return (dest);
+}
+
 void *ft_memmove(void *dest, const void *src, size_t len)
 {
-    int count = 0;
-    while (len > 0)
-    {
-        ft_memcpy(dest, src, 1);
-        dest++;
-        src++;
-        len--;
-        count++;
-    }
-    while (count > 0)
-    {
-        dest--;
-        count--;   
-    }
+    if (dest < src && dest + len > src)
+        ft_memrcpy(dest, src);
+    else
+        ft_memcpy(dest, src, len);     
     return dest;
 }
 
-// int main(void)
-// {
-//     char *dest = (char *)malloc(10 * sizeof(char));
-//     const char *src = "42Tokyo";
+int main(void)
+{
+    char *dest = "42Tokyo";
+    const char *src = dest + 3;
 
-//     char *result = ft_memmove(dest, src, 5);
-//     printf("dest: %s, src: %s, result: %s\n", dest, src, result);
+    char *result = ft_memmove(dest, src, 2);
+    printf("dest: %s, src: %s, result: %s\n", dest, src, result);
     
-//     return 0;
-// }
+    return 0;
+}
