d4efa39d21d86ad838a80426806a5edafa501a4a
rnomoto <r.nomoto12345@gmail.com>
2024-09-14 16:53:44 +0900

    you don't have to think about if n is larger than strlen(s)

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 15:12:46 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/09/14 15:28:57 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/09/14 16:51:51 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,29 @@ void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*s_cast;
 
+	c &= 0xff;
 	s_cast = (unsigned char *)s;
-	while (*s_cast && n > 0)
+	if (n == 0)
+		return (NULL);
+	while (n > 0)
 	{
 		if (*s_cast == c)
 			return (s_cast);
 		s_cast++;
 		n--;
 	}
+	if (*s_cast == c && c == '\0')
+		return (s_cast);
 	return (NULL);
 }
 
 // int main(void)
 // {
-//     const char s[] = "Hello World!";
+//     char s[] = {0, 1, 2 ,3 ,4 ,5};
 
 //     printf("s: %s\n", s);
 
-//     char *result = ft_memchr(s, 'r', 5);
+//     char *result = ft_memchr(s, 0, 0);
 //     printf("result: %s\n", result);
 
 //     return (0);
