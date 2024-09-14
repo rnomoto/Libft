c8107d9fc6454c473f3c83db16a6d80a7c8c2ef0
rnomoto <r.nomoto12345@gmail.com>
2024-09-14 14:17:43 +0900

    memmove

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 15:28:22 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/08/30 16:22:23 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/09/14 14:17:25 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 void	*ft_memset(void *str, int c, size_t n);
 void	*ft_bzero(void *str, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t len);
 
 #endif
