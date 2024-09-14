13585e26aea353e01de68e2be307847b81b852a8
rnomoto <r.nomoto12345@gmail.com>
2024-09-14 20:14:06 +0900

    do not finish copying even if null come

---
+++ ft_strdup.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/14 19:51:01 by rnomoto           #+#    #+#             */
+/*   Updated: 2024/09/14 20:10:37 by rnomoto          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*s2;
+	size_t	index;
+
+	index = 0;
+	s2 = (char *)malloc((ft_strlen(s1) + 1) * sizeof(char));
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	while (index < ft_strlen(s1))
+	{
+		s2[index] = s1[index];
+		index++;
+	}
+	s2[index] = '\0';
+	return (s2);
+}
+
+// int main(void)
+// {
+//     char *result = ft_strdup("Hello World!");
+//     printf("%s\n", result);
+
+//     free(result);
+//     return (0);
+// }
