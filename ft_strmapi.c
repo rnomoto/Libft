dde83f05431366212023b95235b0f40922061b6c
rnomoto <r.nomoto12345@gmail.com>
2024-09-19 00:43:56 +0900

    function pointer is simple

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -3,50 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rnomoto <rnomoto@student.42tokyo.j>        +#+  +:+       +#+        */
+/*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 23:36:17 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/09/19 00:28:34 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/09/19 00:41:46 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	size_t index = 0;
-	char *result = (char *)ft_calloc(sizeof(char), ft_strlen(s));
-	if (result == NULL)
-		return NULL;
+	unsigned int	index;
+	char			*result;
 
+	index = 0;
+	result = (char *)ft_calloc(sizeof(char), ft_strlen(s) + 1);
+	if (result == NULL)
+		return (NULL);
 	while (s[index] != '\0')
 	{
-		result[index] = f;
+		result[index] = f(index, s[index]);
 		index++;
 	}
-	return result;
+	return (result);
 }
 
-char func(unsigned int n, char c)
+char	func(unsigned int n, char c)
 {
-	char result;
+	char	result;
 
 	(void)n;
 	result = c;
-
-	return result;
+	return (result);
 }
 
-int main(void)
-{
-	char *result;
-	char *s = "AAAAAAAAAA";
-	unsigned int n = 0;
-	char c = 'B';
-	char (*f)(unsigned int, char);
+// int main(void)
+// {
+// 	char *s = "AAAAAAAAAA";
+// 	char (*f)(unsigned int, char);
 
-	f = func;
-	result = ft_strmapi(s, (*f)(n, c));
+// 	f = func;
+// 	char *result = ft_strmapi(s, f);
+// 	printf("%s\n", s);
+// 	printf("%s\n", result);
 
-	return 0;
-}
+// 	return (0);
+// }
