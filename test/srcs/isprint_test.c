aff086e96e8ae90dc6851f276188cc470c754e71
rnomoto <r.nomoto12345@gmail.com>
2024-10-27 13:47:15 +0900

    isprint test

---
+++ test/srcs/isprint_test.c
@@ -0,0 +1,122 @@
+#include "../../libft.h"
+#include "../test.h"
+
+int isprint_test(void)
+{
+	int count = 0;
+	int a;
+	int b;
+	
+	printf("argument: '0'\n");
+	a = ft_isprint('0');
+	b = isprint('0');
+	printf("ft_isprint: %d\n", a);
+	printf("isprint   : %d\n", b);
+	if (a == b)
+		printf("OK\n");
+	else if (a != b)
+	{
+		printf("KO\n");
+		count++;
+	}
+	printf("\n");
+
+	printf("argument: 'a'\n");
+	a = ft_isprint('a');
+	b = isprint('a');
+	printf("ft_isprint: %d\n", a);
+	printf("isprint   : %d\n", b);
+	if (a == b)
+		printf("OK\n");
+	else if (a != b)
+	{
+		printf("KO\n");
+		count++;
+	}
+	printf("\n");
+
+	printf("argument: ' '\n");
+	a = ft_isprint(' ');
+	b = isprint(' ');
+	printf("ft_isprint: %d\n", a);
+	printf("isprint   : %d\n", b);
+	if (a == b)
+		printf("OK\n");
+	else if (a != b)
+	{
+		printf("KO\n");
+		count++;
+	}
+	printf("\n");
+
+	printf("argument: '\\n'\n");
+	a = ft_isprint('\n');
+	b = isprint('\n');
+	printf("ft_isprint: %d\n", a);
+	printf("isprint   : %d\n", b);
+	if (a == b)
+		printf("OK\n");
+	else if (a != b)
+	{
+		printf("KO\n");
+		count++;
+	}
+	printf("\n");
+
+	printf("argument: '\\0'\n");
+	a = ft_isprint('\0');
+	b = isprint('\0');
+	printf("ft_isprint: %d\n", a);
+	printf("isprint   : %d\n", b);
+	if (a == b)
+		printf("OK\n");
+	else if (a != b)
+	{
+		printf("KO\n");
+		count++;
+	}
+	printf("\n");
+
+	printf("argument: '\\x80'\n");
+	a = ft_isprint('\\x80');
+	b = isprint('\\x80');
+	printf("ft_isprint: %d\n", a);
+	printf("isprint   : %d\n", b);
+	if (a == b)
+		printf("OK\n");
+	else if (a != b)
+	{
+		printf("KO\n");
+		count++;
+	}
+	printf("\n");
+
+	printf("argument: EOF\n");
+	a = ft_isprint(EOF);
+	b = isprint(EOF);
+	printf("ft_isprint: %d\n", a);
+	printf("isprint   : %d\n", b);
+	if (a == b)
+		printf("OK\n");
+	else if (a != b)
+	{
+		printf("KO\n");
+		count++;
+	}
+	printf("\n");
+
+
+	if (count == 0)
+		printf("ALL OK\n");
+	else if (count == 1)
+	{
+		printf("1 test failed.\n");
+		return 1;
+	}
+	else if (count > 1)
+	{
+		printf("%d tests failed.\n", count);
+		return 1;
+	}
+	return 0;
+}
