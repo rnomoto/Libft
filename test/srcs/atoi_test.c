554fb0ba8d59fc7ceb90e73e9c7b4d55d67910c1
rnomoto <r.nomoto12345@gmail.com>
2024-10-29 10:57:01 +0900

    add tester

---
+++ test/srcs/atoi_test.c
@@ -0,0 +1,258 @@
+#include "../../libft.h"
+#include "../test.h"
+
+int atoi_test(void)
+{
+    int count = 0;
+
+    char *tmp = (char *)malloc(20 * sizeof(char));
+    strlcpy(tmp, "0123456789", 20);
+    printf("str: %s\n", tmp);
+    int ft = ft_atoi(tmp);
+    int ld = atoi(tmp);
+    printf("ft_atoi: %d\n", ft);
+    printf("   atoi: %d\n", ld);
+    if (ft == ld)
+        printf("OK\n");
+    else
+    {
+        printf("KO\n");
+        count++;
+    }
+    free(tmp);
+    printf("\n");
+
+    tmp = (char *)malloc(20 * sizeof(char));
+    strlcpy(tmp, "-12345", 20);
+    printf("str: %s\n", tmp);
+    ft = ft_atoi(tmp);
+    ld = atoi(tmp);
+    printf("ft_atoi: %d\n", ft);
+    printf("   atoi: %d\n", ld);
+    if (ft == ld)
+        printf("OK\n");
+    else
+    {
+        printf("KO\n");
+        count++;
+    }
+    free(tmp);
+    printf("\n");
+
+    tmp = (char *)malloc(20 * sizeof(char));
+    strlcpy(tmp, "-2147483648", 20);
+    printf("str: %s\n", tmp);
+    ft = ft_atoi(tmp);
+    ld = atoi(tmp);
+    printf("ft_atoi: %d\n", ft);
+    printf("   atoi: %d\n", ld);
+    if (ft == ld)
+        printf("OK\n");
+    else
+    {
+        printf("KO\n");
+        count++;
+    }
+    free(tmp);
+    printf("\n");
+
+    tmp = (char *)malloc(20 * sizeof(char));
+    strlcpy(tmp, "-2147483649", 20);
+    printf("str: %s\n", tmp);
+    ft = ft_atoi(tmp);
+    ld = atoi(tmp);
+    printf("ft_atoi: %d\n", ft);
+    printf("   atoi: %d\n", ld);
+    if (ft == ld)
+        printf("OK\n");
+    else
+    {
+        printf("KO\n");
+        count++;
+    }
+    free(tmp);
+    printf("\n");
+
+    tmp = (char *)malloc(20 * sizeof(char));
+    strlcpy(tmp, "", 20);
+    printf("str: %s\n", tmp);
+    ft = ft_atoi(tmp);
+    ld = atoi(tmp);
+    printf("ft_atoi: %d\n", ft);
+    printf("   atoi: %d\n", ld);
+    if (ft == ld)
+        printf("OK\n");
+    else
+    {
+        printf("KO\n");
+        count++;
+    }
+    free(tmp);
+    printf("\n");
+
+    tmp = (char *)malloc(20 * sizeof(char));
+    strlcpy(tmp, "+-++12345", 20);
+    printf("str: %s\n", tmp);
+    ft = ft_atoi(tmp);
+    ld = atoi(tmp);
+    printf("ft_atoi: %d\n", ft);
+    printf("   atoi: %d\n", ld);
+    if (ft == ld)
+        printf("OK\n");
+    else
+    {
+        printf("KO\n");
+        count++;
+    }
+    free(tmp);
+    printf("\n");
+
+    tmp = (char *)malloc(20 * sizeof(char));
+    strlcpy(tmp, "3.1415", 20);
+    printf("str: %s\n", tmp);
+    ft = ft_atoi(tmp);
+    ld = atoi(tmp);
+    printf("ft_atoi: %d\n", ft);
+    printf("   atoi: %d\n", ld);
+    if (ft == ld)
+        printf("OK\n");
+    else
+    {
+        printf("KO\n");
+        count++;
+    }
+    free(tmp);
+    printf("\n");
+
+    tmp = (char *)malloc(20 * sizeof(char));
+    strlcpy(tmp, "0x1A", 20);
+    printf("str: %s\n", tmp);
+    ft = ft_atoi(tmp);
+    ld = atoi(tmp);
+    printf("ft_atoi: %d\n", ft);
+    printf("   atoi: %d\n", ld);
+    if (ft == ld)
+        printf("OK\n");
+    else
+    {
+        printf("KO\n");
+        count++;
+    }
+    free(tmp);
+    printf("\n");
+
+    tmp = (char *)malloc(20 * sizeof(char));
+    strlcpy(tmp, "9223372036854775807", 20);
+    printf("str: %s\n", tmp);
+    ft = ft_atoi(tmp);
+    ld = atoi(tmp);
+    printf("ft_atoi: %d\n", ft);
+    printf("   atoi: %d\n", ld);
+    if (ft == ld)
+        printf("OK\n");
+    else
+    {
+        printf("KO\n");
+        count++;
+    }
+    free(tmp);
+    printf("\n");
+
+    tmp = (char *)malloc(20 * sizeof(char));
+    strlcpy(tmp, "9223372036854775808", 20);
+    printf("str: %s\n", tmp);
+    ft = ft_atoi(tmp);
+    ld = atoi(tmp);
+    printf("ft_atoi: %d\n", ft);
+    printf("   atoi: %d\n", ld);
+    if (ft == ld)
+        printf("OK\n");
+    else
+    {
+        printf("KO\n");
+        count++;
+    }
+    free(tmp);
+    printf("\n");
+
+    tmp = (char *)malloc(21 * sizeof(char));
+    strlcpy(tmp, "-9223372036854775808", 21);
+    printf("str: %s\n", tmp);
+    ft = ft_atoi(tmp);
+    ld = atoi(tmp);
+    printf("ft_atoi: %d\n", ft);
+    printf("   atoi: %d\n", ld);
+    if (ft == ld)
+        printf("OK\n");
+    else
+    {
+        printf("KO\n");
+        count++;
+    }
+    free(tmp);
+    printf("\n");
+
+    tmp = (char *)malloc(21 * sizeof(char));
+    strlcpy(tmp, "-9223372036854775810", 21);
+    printf("str: %s\n", tmp);
+    ft = ft_atoi(tmp);
+    ld = atoi(tmp);
+    printf("ft_atoi: %d\n", ft);
+    printf("   atoi: %d\n", ld);
+    if (ft == ld)
+        printf("OK\n");
+    else
+    {
+        printf("KO\n");
+        count++;
+    }
+    free(tmp);
+    printf("\n");
+
+    tmp = (char *)malloc(21 * sizeof(char));
+    strlcpy(tmp, "18446744073709551615", 21);
+    printf("str: %s\n", tmp);
+    ft = ft_atoi(tmp);
+    ld = atoi(tmp);
+    printf("ft_atoi: %d\n", ft);
+    printf("   atoi: %d\n", ld);
+    if (ft == ld)
+        printf("OK\n");
+    else
+    {
+        printf("KO\n");
+        count++;
+    }
+    free(tmp);
+    printf("\n");
+
+    // tmp = NULL;
+    // printf("str: NULL\n");
+    // ft = ft_atoi(tmp);
+    // ld = atoi(tmp);
+    // printf("ft_atoi: %d\n", ft);
+    // printf("   atoi: %d\n", ld);
+    // if (ft == ld)
+    //     printf("OK\n");
+    // else
+    // {
+    //     printf("KO\n");
+    //     count++;
+    // }
+    // free(tmp);
+    // printf("\n");
+
+    if (count == 0)
+		printf("ALL OK\n");
+	else if (count == 1)
+	{
+		printf("1 test failed.\n");
+		return 1;
+	}
+	else if (count > 1)
+	{
+		printf("%d tests failed.\n", count);
+		return 1;
+	}
+	return 0;
+}
---
+++ test/srcs/strdup_test.c
@@ -0,0 +1,100 @@
+#include "../../libft.h"
+#include "../test.h"
+
+int strdup_test(void)
+{
+    int count = 0;
+
+    char *tmp = (char *)malloc(13 * sizeof(char));
+    strlcpy(tmp, "Hello World!", 13);
+    printf("arg: \"%s\"\n", tmp);
+    char *ret_ft = ft_strdup(tmp);
+    char *ret_lb = strdup(tmp);
+    printf("ft_strdup: %s\n", ret_ft);
+    printf("   strdup: %s\n", ret_lb);
+    if (ret_ft == NULL && ret_lb == NULL)
+        printf("OK\n");
+    else if (strncmp(ret_ft, ret_lb, 13) == 0)
+        printf("OK\n");
+    else
+    {
+        printf("KO\n");
+        count++;
+    }
+    free(tmp);
+    free(ret_ft);
+    free(ret_lb);
+    printf("\n");
+
+    tmp = (char *)malloc(13 * sizeof(char));
+    strlcpy(tmp, "", 13);
+    printf("arg: \"%s\"\n", tmp);
+    ret_ft = ft_strdup(tmp);
+    ret_lb = strdup(tmp);
+    printf("ft_strdup: %s\n", ret_ft);
+    printf("   strdup: %s\n", ret_lb);
+    if (ret_ft == NULL && ret_lb == NULL)
+        printf("OK\n");
+    else if (strncmp(ret_ft, ret_lb, 13) == 0)
+        printf("OK\n");
+    else
+    {
+        printf("KO\n");
+        count++;
+    }
+    free(tmp);
+    free(ret_ft);
+    free(ret_lb);
+    printf("\n");
+
+    printf("arg: \"Hello\\0World!\"\n");
+    ret_ft = ft_strdup("Hello\0World!");
+    ret_lb = strdup("Hello\0World!");
+    printf("ft_strdup: %s\n", ret_ft);
+    printf("   strdup: %s\n", ret_lb);
+    if (ret_ft == NULL && ret_lb == NULL)
+        printf("OK\n");
+    else if (strncmp(ret_ft, ret_lb, 13) == 0)
+        printf("OK\n");
+    else
+    {
+        printf("KO\n");
+        count++;
+    }
+    free(ret_ft);
+    free(ret_lb);
+    printf("\n");
+
+    // printf("arg: NULL\n");
+    // ret_ft = ft_strdup(NULL);
+    // ret_lb = strdup(NULL);
+    // printf("ft_strdup: %s\n", ret_ft);
+    // printf("   strdup: %s\n", ret_lb);
+    // if (ret_ft == NULL && ret_lb == NULL)
+    //     printf("OK\n");
+    // else if (strncmp(ret_ft, ret_lb, 13) == 0)
+    //     printf("OK\n");
+    // else
+    // {
+    //     printf("KO\n");
+    //     count++;
+    // }
+    // free(tmp);
+    // free(ret_ft);
+    // free(ret_lb);
+    // printf("\n");
+
+    if (count == 0)
+		printf("ALL OK\n");
+	else if (count == 1)
+	{
+		printf("1 test failed.\n");
+		return 1;
+	}
+	else if (count > 1)
+	{
+		printf("%d tests failed.\n", count);
+		return 1;
+	}
+	return 0;
+}
