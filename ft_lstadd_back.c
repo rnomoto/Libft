0e32c883b7cfae4e207cc80f0b381a3d11c49222
rnomoto <r.nomoto12345@gmail.com>
2024-11-02 17:02:39 +0900

    comment added

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,21 +6,27 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 09:30:24 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/10/30 15:05:55 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/11/02 16:58:32 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+If a specific behavior is not written in subject, 
+it should handle same as its similar functions. 
+(return NULL, do nothing and comment as undefined behavior,etc.)
+*/
+
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
 
-	if (*lst != NULL && new != NULL)
+	if (*lst != NULL)
 	{
 		last = ft_lstlast(*lst);
 		last->next = new;
 	}
-	else if (new != NULL)
+	else
 		*lst = new;
 }
