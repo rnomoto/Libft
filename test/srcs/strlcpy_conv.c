e2a57c9fefee9ab25ddc5ff1ed27afb6976ddb69
rnomoto <r.nomoto12345@gmail.com>
2025-01-25 21:56:22 +0900

    test moved

--- test/srcs/strlcpy_conv.c
+++
@@ -1,9 +0,0 @@
-#include "../../libft.h"
-#include "../test.h"
-
-size_t	strlcpy(char *dst, const char *src, size_t size)
-{
-    size_t result = ft_strlcpy(dst, src, size);
-
-    return result;
-}
